d59aedcb2c8cac9e9c156c7c17bbe24965fea5f6
Henry Sipp <henrysipp@henrys-macbook-pro.local>
2022-05-09 01:10:47 +0000

    prettier config

--- src/routes/posts.routes.ts
+++ src/routes/posts.routes.ts
@@ -20,7 +20,6 @@ router.get('/:id', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-  
   const { title, body } = req.body 
   const post = await prisma.post.create({
     data: {
